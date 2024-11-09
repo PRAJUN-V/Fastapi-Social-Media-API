9a76cc02ab8f71f3d34fb51c4340e5cd3dbfd024
prajun v <prajunvelandi@gmail.com>
2024-11-09 07:28:40 +0530

    get, post and data validation using pydantic is implemented

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # FastAPI Social Media API
 
 A simple, scalable social media API built with FastAPI. This API provides endpoints for core social media functionality, including user authentication, posting content, managing profiles, and interacting with other users.
+
+## Getting Started
+### Prerequisites
+- **Python 3.8+**
+- **FastAPI**
+
+```markdown
+pip install fastapi[all]
+```
+
+
+
+
+- [Tutorial Followed](https://lnkd.in/gDjt_Gji)
+- [Official Documentation](https://fastapi.tiangolo.com/tutorial/)
--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True # if user doesn't provide it should be defaulted True
+    rating: Optional['int'] = None # if rating is not given it return None value.
+
+@app.get("/")
+def root(): # this is path operation function we can name it anything, but ideally it is better to use a descriptive name.
+    return {"message": "Hello world"}
+
+@app.get("/posts")
+def get_post():
+    return {"data": "This is your post."}
+
+@app.post("/post-without-data-validation")
+def create_post_without_data_validation(payload: dict = Body(...)): # Data sending without data validation, here Body from request is converted to dict and store in a variable named payload
+    print(payload)
+    return payload
+
+@app.post("/create-post")
+def create_post(new_post: Post): # Data sending with data validation
+    print(new_post)
+    return {"data": "new post"}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
